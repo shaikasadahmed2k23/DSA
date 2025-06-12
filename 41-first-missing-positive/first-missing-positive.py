ca37271fe1666f4e93cf944d98a67bc98835c2ad
SHAIK ASAD AHMED <155294325+shaikasadahmed2k23@users.noreply.github.com>
2025-06-12 14:30:06 +0530

    Time: 11 ms (99.62%) | Memory: 29.5 MB (27.58%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,22 +1,33 @@
+# class Solution:
+#     def firstMissingPositive(self, nums: List[int]) -> int:
+#         # 1) Replcaing -ve elements with n+2
+#         n = len(nums)
+#         # x = n+2
+#         for i in range(n):
+#             # n = len(nums)
+#             if nums[i]<=0 or nums[i]>n:
+#                 nums[i] = n+1
+#         # print(nums)
+#         for i in range(n):
+#             v = abs(nums[i])
+#             if v<=n and nums[v-1]>0:
+#                 nums[v-1] = -nums[v-1] 
+#             # nums[nums[i]-1] = (-1)*(nums[i])
+#         # print(nums)
+#         for j in range(n):
+#             if nums[j]>0:
+#                 return j+1
+#         return n+1
+#         # return 1
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # 1) Replcaing -ve elements with n+2
         n = len(nums)
-        # x = n+2
-        for i in range(n):
-            # n = len(nums)
-            if nums[i]<=0 or nums[i]>n:
-                nums[i] = n+1
-        # print(nums)
-        for i in range(n):
-            v = abs(nums[i])
-            if v<=n and nums[v-1]>0:
-                nums[v-1] = -nums[v-1] 
-            # nums[nums[i]-1] = (-1)*(nums[i])
-        # print(nums)
-        for j in range(n):
-            if nums[j]>0:
-                return j+1
-        return n+1
-        # return 1
+        f = [False] * (n + 1)
+        for i in nums:
+            if i > 0 and i <= n:
+                f[i] = True
+        for j in range(1, n + 1):
+            if not f[j]:
+                return j
+        return n + 1 
         
