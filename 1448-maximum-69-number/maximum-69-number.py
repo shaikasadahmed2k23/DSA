1f4eae1b0199bff8ea2cd02a9289ac86adeb5d4f
SHAIK ASAD AHMED <155294325+shaikasadahmed2k23@users.noreply.github.com>
2025-08-16 23:22:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (76.77%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.py
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -1,16 +1,8 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        res = []
-        n1 = list(str(num))  # Convert to list to make it mutable
-        for i in range(len(n1)):
-            original = n1[i]
-            if original == '6':
-                n1[i] = '9'  # Change 6 to 9
-                res.append(int(''.join(n1)))
-                n1[i] = original  # revert back for next iteration
-            elif original == '9':
-                n1[i] = '6'  # Change 9 to 6
-                res.append(int(''.join(n1)))
-                n1[i] = original  # revert back
-        res.append(num)  # Also consider original number
-        return max(res)
+        s1 = list(str(num))
+        for i in range(len(s1)):
+            if s1[i] == '6':
+                s1[i] = '9'
+                break 
+        return int(''.join(s1))
