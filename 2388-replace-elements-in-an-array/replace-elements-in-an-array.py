5081dfe61d234a40d481467711c21e2f5d67a37a
SHAIK ASAD AHMED <155294325+shaikasadahmed2k23@users.noreply.github.com>
2025-08-20 14:24:22 +0530

    Time: 63 ms (68.52%) | Memory: 70 MB (81.94%) - LeetSync

---
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        # print(opera
+        f = {}
+        for i, num in enumerate(nums):
+            f[num] = i
+        # print(f)
+        for old, new in operations:
+            if old in f:
+                idx = f.pop(old)
+                f[new] = idx
+        # print(f)
+        res = [0] * len(nums)
+        # result = [0] * len(nums)
+        for num, idx in f.items():
+            res[idx] = num
+
+        return res
