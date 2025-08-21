eec48668142fdc945b5bcdac7adfc925c5ee8217
SHAIK ASAD AHMED <155294325+shaikasadahmed2k23@users.noreply.github.com>
2025-08-22 00:34:08 +0530

    Time: 3 ms (86.63%) | Memory: 18.2 MB (52.21%) - LeetSync

---
+++ 415-add-strings/add-strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        n1 = num1[::-1]
+        n2 = num2[::-1]
+        res = []
+        c = 0
+        ml = max(len(n1), len(n2))
+        
+        for i in range(ml):
+            d1 = int(n1[i]) if i < len(n1) else 0
+            d2 = int(n2[i]) if i < len(n2) else 0
+
+            total = d1 + d2 + c
+            res.append(str(total % 10))
+            c = total // 10
+
+        if c:
+            res.append(str(c))
+        
+        return ''.join(res[::-1])
