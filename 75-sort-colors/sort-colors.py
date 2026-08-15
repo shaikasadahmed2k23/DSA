c7a0f6379fee4b107f674c20ff22bc7c6fc2abdb
SHAIK ASAD AHMED <155294325+shaikasadahmed2k23@users.noreply.github.com>
2026-08-15 19:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (90.95%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,7 +1,40 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+class Solution(object):
+    def sortColors(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
-        return nums
+        l = 0
+        m = 0
+        h = len(nums) - 1
+        while m <= h:
+            if nums[m] == 0:
+                nums[l], nums[m] = nums[m], nums[l]
+                l += 1
+                m += 1
+            elif nums[m] == 1:
+                m += 1
+            elif nums[m] == 2:
+                nums[m], nums[h] = nums[h], nums[m]
+                h -= 1
+
+
+        # # return nums.sort
+        # z = []
+        # o = []
+        # t = []
+        # for i in nums:
+        #     if i == 0:
+        #         z.append(0)
+        #     elif i == 1:
+        #         o.append(1)
+        #     else:
+        #         t.append(2)
+        # # print(z,o,t)
+        # z.extend(o)
+        # z.extend(t)
+        # # return str(z).replace(" ", "")
+        # # print(z)
+        # nums[:] = z
+        
+        # # return z
