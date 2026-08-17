523f17a3157d5d832995669f38be985e8dbd3bb7
SHAIK ASAD AHMED <155294325+shaikasadahmed2k23@users.noreply.github.com>
2026-08-17 23:33:54 +0530

    Time: 15 ms (71.97%) | Memory: 12.7 MB (75.70%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,25 +1,42 @@
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s1 = ""
-        for i in s:
-            # print(i)
-            if i.isalnum():
-                s1 += i.lower()
-        # # print(s1)
-        s2 = s1[::-1]
-        # if s1 == s1[::-1]:
-        #     return True
-        # else:
-        #     return False
         l = 0
-        h = len(s1) - 1
-        for i in range(len(s2)//2):
-            if s2[l] != s2[h]:
+        h = len(s) - 1
+
+        while l < h:
+            if not s[l].isalnum():
+                l += 1
+                continue
+
+            if not s[h].isalnum():
+                h -= 1
+                continue
+
+            if s[l].lower() != s[h].lower():
                 return False
+
             l += 1
             h -= 1
+
         return True
+
+                
+        # s1 = ""
+        # for i in s:
+        #     # print(i)
+        #     if i.isalnum():
+        #         s1 += i.lower()
+        # # # print(s1)
+        # s2 = s1[::-1]
+        # # if s1 == s1[::-1]:
+        # #     return True
+        # # else:
+        # #     return False
+        # l = 0
+        # h = len(s1) - 1
+        # for i in range(len(s2)//2):
+        #     if s2[l] != s2[h]:
+        #         return False
+        #     l += 1
+        #     h -= 1
+        # return True
