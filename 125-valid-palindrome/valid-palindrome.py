20d5c48c9b37e40b038e640dc230b4de483380f6
SHAIK ASAD AHMED <155294325+shaikasadahmed2k23@users.noreply.github.com>
2026-08-17 23:15:09 +0530

    Time: 241 ms (15.01%) | Memory: 13.8 MB (25.46%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,13 +1,25 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         s1 = ""
         for i in s:
+            # print(i)
             if i.isalnum():
-                s1 = s1 + i
-        # print(s1)
-        s1 = s1.lower()
-        # print(s1)
-        if s1 == s1[::-1]:
-            return True
-        else:
-            return False
+                s1 += i.lower()
+        # # print(s1)
+        s2 = s1[::-1]
+        # if s1 == s1[::-1]:
+        #     return True
+        # else:
+        #     return False
+        l = 0
+        h = len(s1) - 1
+        for i in range(len(s2)//2):
+            if s2[l] != s2[h]:
+                return False
+            l += 1
+            h -= 1
+        return True
